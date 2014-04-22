2c4062b8018d2cb2f0f35358cb821e728e2b36e0
facundo rubin <facundonahuel9@gmail.com>
2014-04-22 18:40:12 -0300

    html

--- construccion.php
+++ construccion.php
@@ -3,8 +3,9 @@ include 'header.php';
 	echo '<article id="contenido">
 						<h2 class="titulo">Segundo Proyecto</h2>
 						<p>
-							En costrucciòn...
+							En costrucción...
 						</p>
+						<img width="700px"src="img/construccion.jpg" alt="construccion">
 		</article>';
 include 'footer.php';
 ?>
---
+++ contacto.php
@@ -0,0 +1,29 @@
+<?php 
+include 'header.php';
+?>
+<article id="contenido">
+	<h2 class="titulo">Envianos tu consulta:</h2>
+		<form>
+		<p>Nombre:</p>
+		<input type="text" name="nombre"><br>
+		<p>Apellido:</p> 
+		<input type="text" name="apellido"><br>
+		<p>Escriba su consulta:</p>
+		<textarea rows="10" cols="30" name="consulta">
+		</textarea>
+		<p>Correo:</p> 
+		<input type="text" name="email">
+		</form>
+	<h2 class="titulo">Nuestro local</h2>
+		<p>Los invitamos a visitar nuestro local ubicado en pleno centro de la ciudad de Corrientes Capital.</p>
+		<p>Junin 985</p>
+		<p>Entre Buenos Aires y Tucuman</p>
+		<p>O ante alguna consulta nuestro telefono es:</p>
+		<p>+54 (3794) 444452</p>
+	<h2 class="titulo">Redes Sociales:</h2>
+		<p><a href="#" class="icono-facebook"> Facebook</a></p>
+	  	<p><a href="#" class="icono-twitter"> Twitter</a></p>
+</articule>
+<?php
+include 'footer.php';
+?>
